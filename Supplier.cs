c86b9f635cf9ee71605e637094c4057a0b16d302
janshu09 <36918357+janshu09@users.noreply.github.com>
2018-02-28 17:54:49 +0530

    Add files via upload

---
+++ Supplier.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProjectMVC.Models
+{
+    public class Supplier
+    {
+        [Key]
+        public int SupplierID { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength =5)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 5)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(40, MinimumLength = 10)]
+        public string CompanyName { get; set; }
+    }
+}
---
+++ SupplierController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectMVC.Models;
+using ProjectMVC.ViewModel;
+using ProjectMVC.DataAccessLayer;
+
+namespace ProjectMVC.Controllers
+{
+    public class SupplierController : Controller
+    {
+        // GET: Supplier
+        public ActionResult DisplaySupplier()
+        {
+            var ListSupplier = GetSupplier();
+            return View(ListSupplier);
+        }
+        public List<Supplier> GetSupplier()
+        {
+            MVCDal dal = new MVCDal();
+            return dal.Suppliers.ToList();
+        }
+        public ActionResult AddNew()
+        {
+            return View();
+        }
+        public ActionResult EditSupplier(int id)
+        {
+            var supplier = GetSupplier().SingleOrDefault(s => s.SupplierID == id);
+            if (supplier == null)
+            {
+                return HttpNotFound();
+            }
+                return View("AddNew",supplier);
+        }
+        public ActionResult Save(Supplier supplier)
+        {
+            MVCDal obj = new MVCDal();
+            if(supplier.SupplierID==0)
+            {
+                obj.Suppliers.Add(supplier);
+            }
+            else
+            {
+                var s = obj.Suppliers.Find(supplier.SupplierID);
+                s.FirstName = supplier.FirstName;
+                s.LastName = supplier.LastName;
+                s.CompanyName = supplier.CompanyName;
+            }
+            obj.SaveChanges();
+            return RedirectToAction("DisplaySupplier","Supplier");
+        }
+    }
+}
